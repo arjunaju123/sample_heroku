49a29f3682637a9bf360cdc296dcaed137267c29
Arjun S <54721arjun@gmail.com>
2021-05-16 16:33:37 +0530

    second

---
+++ visualization.py
@@ -0,0 +1,28 @@
+#import libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+
+#matplotlib.use('Agg')
+import seaborn as sns
+#Remove Warnings
+st.balloons()
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.title("Tips_Dataset")
+
+#import dataset
+df = pd.read_csv('tips.csv')
+#First thirty rows
+tips = df.head(10)
+#Display the table
+st.table(tips)
+
+#pairplot
+st.subheader("Pairplot")
+sns.pairplot(tips,hue='sex',palette='rainbow')
+st.pyplot()
+
+#Correation
+st.subheader("Heatmap")
+sns.heatmap(tips.corr(),cmap='coolwarm',annot=True)
+st.pyplot()
